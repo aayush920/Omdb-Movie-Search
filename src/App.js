0c69acf4baaf7ad639d2624baf1ad5d6b4eacd44
Aayush <saayush05@gmail.com>
2022-08-05 01:54:24 +0530

    Changed HTTP to HTTPS

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [movies,setMovies]=useState([]);
   const [clicked,setClicked]=useState(false);
   async function getMovie(search){
-      const response=await fetch(`http://www.omdbapi.com/?apikey=${API}&s=${search}`);
+      const response=await fetch(`https://www.omdbapi.com/?apikey=${API}&s=${search}`);
       const data =response.json();
       return data;
   }
